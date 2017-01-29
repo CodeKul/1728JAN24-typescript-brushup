3028bbf1899d6fdc6d62313925e851a57bf99ff5
aniruddha <aniruddha.kudalkar@gmail.com>
2017-01-29 15:31:53 +0530

    how to add type to variables, functions and function parameters

---
+++ typescript-lang-fundamentals/funda.js
@@ -0,0 +1,12 @@
+var osName = 'android';
+console.log("Os Name is " + osName);
+function whatIsOs() {
+    return osName;
+}
+function thisIsVersion(version) {
+    console.log("Os Version is " + version);
+}
+console.log("How to compile -> tsc yourFile.ts");
+console.log("How to Run -> node yourFile.js");
+whatIsOs();
+thisIsVersion(2);
---
+++ typescript-lang-fundamentals/funda.ts
@@ -0,0 +1,15 @@
+let osName : string = 'android';
+console.log(`Os Name is ${osName}`);
+
+function whatIsOs() : string {
+    return osName;
+}
+
+function thisIsVersion(version : number) {
+    console.log(`Os Version is ${version}`);
+}
+console.log(`How to compile -> tsc yourFile.ts`);
+console.log(`How to Run -> node yourFile.js`);
+
+whatIsOs();
+thisIsVersion(2);
